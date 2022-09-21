3b3e93108b1c894f1d52cb6c34dc6ab6b5a86db1
Rajiv Das <69184715+rajiv3672@users.noreply.github.com>
2022-09-21 14:37:38 +0600

    Minor polishing
    Now I am using \\beta to print beta rather than using β(as previously done).

--- Voltage Divider Bias AC analysis_2 at low freq eq ckt.py
+++ Voltage Divider Bias AC analysis_2 at low freq eq ckt.py
@@ -5,5 +5,5 @@ with schemdraw.Drawing() as d:
     d.config(fontsize=30)
     d += (L1 := elm.Line().left().dot(open=True).label('$v_s$', 'left'))
     d += (C1 := elm.Capacitor().right().at(L1.start).label('$C_1$').dot())
-    d += (R1 := elm.Resistor().down().at(C1.end).label('$R_i=R_1||R_2||$$βr_e$'))
+    d += (R1 := elm.Resistor().down().at(C1.end).label('$R_i=R_1||R_2||$$\\beta r_e$'))# Using \beta causes problem because \b is a library thing so I used \\beta.
     d += (G1 := elm.Ground().at(R1.end))
