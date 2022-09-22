36695f5dd6833dc19384a62dd4cc06e06a41fc1d
Rajiv Das <69184715+rajiv3672@users.noreply.github.com>
2022-09-22 17:38:08 +0600

    New circuits
    Complete circuits for the explanation of the r_e model.

---
+++ r_e model CE ckt_6 tot eq model.py
@@ -0,0 +1,16 @@
+import schemdraw
+import schemdraw.elements as elm
+
+with schemdraw.Drawing() as d:
+    d.config(fontsize=20)
+    d += (L1 := elm.Line().left().dot(open=True).length(2))
+    d += (L3 := elm.Line().down().at(L1.start).length(1))
+    d += (R1 := elm.Resistor().down().at(L3.start).label('$\\beta r_e$'))
+    d += (L2 := elm.Line().left().at(R1.end).dot(open=True).length(2))
+    d += (H1 := elm.Gap().endpoints(L1.end, L2.end).label(('+', '$V_{be}$', '-')))
+    d += (I1 := elm.CurrentLabelInline(direction='in', headwidth=0.15).at(L1.center).right().label('$I_b$').scale(0.1))
+    d += (L4 := elm.Line().length(2).at(R1.end).right())
+    d += (S1 := elm.SourceControlledI().reverse().up().label('$\\beta I_b$', 'bot'))
+    d += (L5 := elm.Line().right().length(2).at(S1.start).dot(open=True))
+    d += (L6 := elm.Line().right().length(2).at(S1.end).dot(open=True))
+    d += (H2 := elm.Gap().endpoints(L5.end, L6.end).label(('+', '$V_{ce}$', '-')))
