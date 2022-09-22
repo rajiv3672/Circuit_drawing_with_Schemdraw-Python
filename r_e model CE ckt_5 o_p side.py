b0925cfb6dae8586eb36cfd91c4b2d7446c470ea
Rajiv Das <69184715+rajiv3672@users.noreply.github.com>
2022-09-22 17:17:39 +0600

    New circuits!!

---
+++ r_e model CE ckt_5 o_p side.py
@@ -0,0 +1,8 @@
+import schemdraw
+import schemdraw.elements as elm
+
+with schemdraw.Drawing() as d:
+    d.config(fontsize=20)
+    d += (L1 := elm.Line().right().dot(open=True).length(2))
+    d += (S1 := elm.SourceControlledI().down().at(L1.start).label('$\\beta I_b$'))
+    d += (L2 := elm.Line().right().at(S1.end).dot(open=True).length(2))
